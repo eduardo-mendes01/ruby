5faec6d50472219387c35db6777878c3ad55bada
eduardo-mendes01 <efiasn@gmail.com>
2026-08-26 23:29:18 -0300

    vetor

---
+++ 11_vetores_arrays.rb
@@ -0,0 +1,19 @@
+vetor = []
+
+#empurrar um valor no array
+vetor.push(67)
+vetor.push(90)
+vetor.push(45)
+
+puts vetor[0]
+
+#substituir um array
+vetor[0] = 1
+
+puts vetor[0]
+
+#remover elemento
+
+vetor.delete(vetor[0])
+
+puts vetor[0]
