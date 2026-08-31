c7835b8f7e21424cebd13a925497fe31ca5969cb
eduardo-mendes01 <efiasn@gmail.com>
2026-08-31 19:52:16 -0300

    mixins

---
+++ modulos/3_b.rb
@@ -0,0 +1,9 @@
+module B
+
+    def b1
+    end
+
+    def b2
+    end
+
+end
---
+++ modulos/4_exemplo.rb
@@ -0,0 +1,11 @@
+require_relative '2_a'
+require_relative '3_b'
+
+class Exemplo
+    include A
+    include B
+
+    def ex1
+
+    end
+end
